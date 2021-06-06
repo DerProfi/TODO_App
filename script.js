6061045a149496c070fcbf2f72ab1048ce823952
Leon Nothegger <nothegger.leon@gmail.com>
2021-06-06 14:02:46 +0200

    Added checkboxes

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var schalter = document.getElementById('lightSwitch');
 var neue = document.getElementById('newTodoText');
-var title = document.getElementById('headerBackground')
+var title = document.getElementById('headerBackground');
+var todos = document.getElementsByClassName('newTodo');
 
 
 schalter.onclick = function() {
     if (schalter.style.backgroundImage = 'url(./images/icon-sun.svg)'){
         document.body.style.backgroundColor = 'hsl(0, 0%, 98%)';
         schalter.style.backgroundImage = 'url(./images/icon-moon.svg)';
-        title.style.background = 'url(./images/bg-desktop-light.jpg)'
+        title.style.background = 'url(./images/bg-desktop-light.jpg)';
+        todos.style.backgroundColor = 'hsl(0, 0%, 98%)';
     }
     else if (schalter.style.backgroundImage = 'url(./images/icon-moon.svg)'){
         document.body.style.backgroundColor = 'black';
@@ -15,16 +17,28 @@ schalter.onclick = function() {
     }
 }
 
-
-var text = document.getElementById("newTodoText");
+const text = document.getElementById("newTodoText");
 
 text.addEventListener('keydown', function(e) {
     if (e.keyCode == 13) {
-        var lastone = document.getElementById("holder")
-        var newBox = document.createElement("Div");
-        newBox.innerHTML = text.value;                   
-        lastone.prepend(newBox);
+        const lastElement = document.getElementById("holder");
+        const newBox = document.createElement("Div");
+        const divCreateNewTodo = '<div>'+'<input type="checkbox" name="newTodo" id="newTodoCheck">'+ text.value +'</div>';
+        newBox.innerHTML = divCreateNewTodo;                   
+        lastElement.before(newBox);
         newBox.classList.add("allTodo");
-        text.value = '';  
+        text.value = '';
     }
 });
+
+const checkbox = document.getElementById("newTodoCheck");
+checkbox.addEventListener('click', function(e) {
+    if (text.style.textDecoration="none"){
+    text.style.textDecoration="line-through"
+}
+    else if (text.style.textDecoration="line-through"){
+        text.style.textDecoration="none"
+    }
+})
+
+
